4baccb71a356b595c9ee17522dfb5abfed9da183
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-05 13:53:44 -0500

    uuuuuuuuuuuuuuuuuu

---
+++ animuu/animuu/Aminal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animuu
+{
+    class Aminal
+    {
+        String species;
+        String name;
+
+        public Aminal (String Name, String Species)
+        {
+            name = Name;
+            species = Species;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Grrrrr.....");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
