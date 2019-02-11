52ca5337e2618d832f8777ebd7c10286bd9612c9
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-11 11:20:01 -0500

    gar & orang

---
+++ animuu/animuu/Garfeild.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animuu
+{
+    class Garfeild
+    {
+        //properties
+        String species;
+        String name;
+
+        //constructor
+        public Garfeild(String Name)
+        {
+            species = "Cat";
+            Name = name;
+        }
+
+        //methods
+        public void MakeSound()
+        {
+            Console.WriteLine("mmmmmm lasagna ~v~");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void BeHappy()
+        {
+            LayOnLap();
+            Console.WriteLine("*takes a nap*");
+        }
+
+        public void LayOnLap()
+        {
+            Console.WriteLine("*cuddles on owners lap :3*");
+        }
+
+        public void SeeLazer()
+        {
+            BeHappy();
+        }
+    }
+}
---
+++ animuu/animuu/OrangeTabby.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animuu
+{
+    class OrangeTabby
+    {
+        //properties
+        String species;
+        String name;
+
+        //constructor
+        public OrangeTabby(String Name)
+        {
+            species = "Cat";
+            Name = name;
+        }
+
+        //methods
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void BeHappy()
+        {
+            LayOnLap();
+            Console.WriteLine("*takes a nap*");
+        }
+
+        public void LayOnLap()
+        {
+            Console.WriteLine("*cuddles on owners lap :3*");
+        }
+
+        public void SeeLazer()
+        {
+            Console.WriteLine("*kitty pounces!*");
+        }
+    }
+}
