d72bbdf07bfb8459459b0a28bf3c1870d4eeaa6f
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-11 11:21:35 -0500

    rrus

---
+++ animuu/animuu/RussianBlue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animuu
+{
+    class RussianBlue
+    {
+        //properties
+        String species;
+        String name;
+
+        //constructor
+        public RussianBlue(String Name)
+        {
+            species = "Cat";
+            Name = name;
+        }
+
+        //methods
+        public void MakeSound()
+        {
+            Console.WriteLine("Hisssssssssssss");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void BeHappy()
+        {
+            Console.WriteLine("purrrrrrrrrr");
+            SeeLazer();
+
+        }
+
+        public void SeeLazer()
+        {
+            Console.WriteLine("*kitty pounces!*");
+        }
+    }
+}
