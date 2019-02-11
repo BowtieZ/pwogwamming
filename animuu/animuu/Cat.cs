862be3b4a079012468aca0a7008c67ede64d193a
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-11 11:11:50 -0500

    cat

---
+++ animuu/animuu/Cat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animuu
+{
+    class Cat
+    {
+        //properties
+        String species;
+        String name;
+
+        //constructor
+        public Cat(String Name)
+        {
+            species = "Cat";
+            Name = name;
+        }
+
+        //methods
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void BeHappy()
+        {
+            Console.WriteLine("purrrrrrrrrr");
+            SeeLazer();
+            MakeSound();
+        }
+
+        public void LayOnLap()
+        {
+            Console.WriteLine("*cuddles on owners lap :3*");
+        }
+
+        public void SeeLazer()
+        {
+            Console.WriteLine("*kitty pounces!*");
+        }
+    }
+}
