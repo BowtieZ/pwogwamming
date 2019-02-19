0069bd74b70a6837de7dc2e9d5a4df3f647ae7f8
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-19 13:52:36 -0500

    dfaerga

--- gradeboonk/gradeboonk/Program.cs
+++ gradeboonk/gradeboonk/Program.cs
@@ -10,6 +10,7 @@ namespace gradeboonk
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ gradeboonk/gradeboonk/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradeboonk
+{
+    class Student
+    {
+        //parameters
+        string FirstName;
+        string LastName;
+        int TardyCount;
+        int AbsentCount;
+        double OverallScore;
+
+        //Constructor 
+        public Student(string firstName, string lastName, long phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TardyCount = 0;
+            AbsentCount = 0;
+
+            
+        }
+
+        //methoods
+
+
+    }
+}
