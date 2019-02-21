f06c5706ab803fc96fe23e2de233118596452900
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-21 09:34:36 -0500

    Update Student.cs

--- gradeboonk/gradeboonk/Student.cs
+++ gradeboonk/gradeboonk/Student.cs
@@ -14,20 +14,42 @@ namespace gradeboonk
         int TardyCount;
         int AbsentCount;
         double OverallScore;
+        long PhoneNumber;
+        string Username;
 
         //Constructor 
-        public Student(string firstName, string lastName, long phoneNumber)
+        public Student(string firstName, string lastName, string username, long phoneNumber) //try to make boolean?
         {
             FirstName = firstName;
             LastName = lastName;
             TardyCount = 0;
             AbsentCount = 0;
-
+            PhoneNumber = phoneNumber;
+            Username = username;
             
         }
 
         //methoods
-
+        
+        Public void markTardy( string username )
+        {
+            TardyCount++;
+        } 
+        
+        Public void markAbsent( string username )
+        {
+            AbsentCount++;
+        }
+        
+        Public int getTardyCount(string username)
+        {
+            return TardyCount;
+        }
+        
+        Public int getAbsentCount(string username)
+        {
+            return AbsentCount;
+        }
 
     }
 }
