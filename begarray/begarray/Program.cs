8efe6ef9a0430cf585ef7bdd0bd4f77840da5a26
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-22 12:58:17 -0500

    ids

--- begarray/begarray/Program.cs
+++ begarray/begarray/Program.cs
@@ -10,6 +10,21 @@ namespace begarray
     {
         static void Main(string[] args)
         {
+            //decleration
+            int[] intArray1 = new int[50];
+            int[] arrayOfNumbers = new int[3];
+            int[] numbersArray = new int[5];
+
+            double[] dblArray = new double[4];
+            double[] arrayOfDoubles = new double[15];
+            double[] playerPoints = new double[39];
+
+            string[] stringArray1 = new string[20];
+            string[] arrayOfStrings = new string[25];
+            string[] wordArray = new string[6];
+
+            //setting values
+
         }
     }
 }
