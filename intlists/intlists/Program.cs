9fdd15e8c85cef5a4971f1b3fee0184cfbf096e8
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-02-25 11:23:54 -0500

    hgggh

---
+++ intlists/intlists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intlists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare
+
+            List<int> intergerList = new List<int>();
+            List<int> listOfInts = new List<int>();
+            List<int> testScores = new List<int>();
+
+            List<double> dbLIst = new List<double>();
+            List<double> listOfDoubles = new List<double>();
+            List<double> playerPoints = new List<double>();
+
+            List<string> stringList = new List<string>();
+            List<string> listOfStrings = new List<string>();
+            List<string> studentName = new List<string>();
+
+            List<int> intList = new List<int>();
+            List<double> dblList = new List<double>();
+            List<string> strList = new List<string>();
+        }
+    }
+}
