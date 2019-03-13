990d66b003d6ff82ed511128b28df77490433777
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-03-13 13:34:48 -0400

    bbbbbbbbbb

--- advarray/advarray/Program.cs
+++ advarray/advarray/Program.cs
@@ -12,10 +12,29 @@ namespace advarray
         {
             //messing around with how to search lists
             Console.WriteLine("enter user");
+            bool Avalibility;
             string Username = Console.ReadLine();
+            //small array for testing, this is what the program needs to search through
             string[] array1 = { "cat", "dog", "carrot", "bird" };
-            string value1 = Array.Find(array1, element => element.StartsWith(Username, StringComparison.Ordinal));
-            if (value1 == null)
+            /*not exactly sure what this line does....but it searches the previously established array for the parameter given (in this case, whatever is in the string value Username):
+            string value1 = Array.Find(array1, element => element.StartsWith(Username, StringComparison.Ordinal));*/
+
+            //this may take more lines to write, but i understand how it works. 
+            for (int i =0; i < array1.Length; i++)
+            {
+                if (array1[i].Equals(Username)) {
+                Avalibility = true;
+                    Console.WriteLine("username unavailible");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("username availible");
+                }
+            }
+
+            //if the value of the array search comes back empty, the username is avalible, but if it matches, it's unavalible
+            /*if (value1 == null)
             {
                 Console.WriteLine("username availible");
                 Console.WriteLine(Username);
@@ -24,7 +43,7 @@ namespace advarray
             {
                 Console.WriteLine("username unavailible");
             }
-            
+            */
             Console.ReadKey();
         }
     }
