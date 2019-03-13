2bf55943002218d872b62718661a5d403103b7dd
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-03-13 12:55:10 -0400

    ffff

---
+++ advarray/advarray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //messing around with how to search lists
+            Console.WriteLine("enter user");
+            string Username = Console.ReadLine();
+            string[] array1 = { "cat", "dog", "carrot", "bird" };
+            string value1 = Array.Find(array1, element => element.StartsWith(Username, StringComparison.Ordinal));
+            if (value1 == null)
+            {
+                Console.WriteLine("username availible");
+                Console.WriteLine(Username);
+            }
+            else
+            {
+                Console.WriteLine("username unavailible");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
