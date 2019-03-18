345bfa00c0b750d5de8354d33840a81407de7da6
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-03-18 13:33:21 -0400

    yyyyyyyyyyyyyyyyyyy

--- advarray/advarray/Program.cs
+++ advarray/advarray/Program.cs
@@ -20,7 +20,7 @@ namespace advarray
             /*not exactly sure what this line does....but it searches the previously established array for the parameter given (in this case, whatever is in the string value Username):
             string value1 = Array.Find(array1, element => element.StartsWith(Username, StringComparison.Ordinal));
 
-            0000000000000000000//this may take more lines to write, but i understand how it works. 00000000000000000000000000000
+            0000000000000000000//this may take more lines to write, but i understand how it works. 000000000000000000000000000000000000000
             for (int i =0; i < array1.Length; i++)
             {
                 if (array1[i].Equals(Username)) {
