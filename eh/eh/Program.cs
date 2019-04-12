48780764f3b2a87f3ab68572b6b074988c06fd2b
BowtieZ <45171591+BowtieZ@users.noreply.github.com>
2019-04-12 12:48:01 -0400

    aa

---
+++ eh/eh/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool open = true;
+            stats NewGame = new stats();
+            Console.WriteLine("Hi and welcome to the Andromeda Idle! here are your current earnings and belongings." );
+            Console.WriteLine("everytime you open the game, you start over. type commands to get quarks and eventually create your universe.");
+            while (open == true)
+            {
+                string action = Console.ReadLine();
+                switch (action)
+                {
+                    case "show resources":
+                        NewGame.Resources();
+                        break;
+                    case "get quarks":
+                        NewGame.Quarks();
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("you now have " + NewGame.getQuarks() + " quarks!");
+                        break;
+                    case "make atoms":
+                        NewGame.Atoms();
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("you now have " + NewGame.getAtoms() + " atoms!");
+                        break;
+                    case "create hydrogen":
+                        NewGame.Hydrogen();
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("you now have" + NewGame.getHydrogen() + " hydrogen");
+                        break;
+                    case "create helium":
+                        NewGame.Helium();
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("you now have " + NewGame.getHelium() + " helium");
+                        break;
+                    case "create star":
+                        NewGame.Star();
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine("you now have " + NewGame.getStar() + " stars!");
+                        Console.WriteLine("Now that you have stars, the aquisition of materials should be atleast a little easier. good luck!");
+                        //somehow figure out how to use delegates.
+                        //NewGame.starResourceCounter();
+                        //edit: actually lets try multithreading it
+                        var ts = new ThreadStart(NewGame.starResourceCounter);
+                        var backgroundThread = new Thread(ts);
+                        backgroundThread.Start();
+                        //threading it actually works!!! stop making it print though, that's confusing. 
+                        break;
+                    case "create planet":
+                        break;
+                    case "create sister stars":
+                        break;
+                    case "create moon":
+                        break;
+                }
+                Console.WriteLine("what would you like to do now?");
+                
+            }
+        }
+    }
+}
---
+++ eh/eh/stats.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eh
+{
+    class stats
+    {
+        //properties
+        double quarks;
+        int atoms;
+        int hydrogen;
+        int helium;
+        double carbon;
+        int oxygen;
+        int star;
+        int planet;
+        int redGiant;
+        int supernova;
+        int nebula;
+        int moon;
+        //constructor
+        public stats()
+        {
+            quarks = 10;
+            atoms = 0;
+            hydrogen = 0;
+            helium = 0;
+            carbon = 0;
+            oxygen = 0;
+            star = 0;
+            planet = 0;
+            redGiant = 0;
+            supernova = 0;
+            nebula = 0;
+            moon = 0;
+        }
+        //methoods
+        //for creating quarks
+        public void Quarks()
+        {
+            Console.WriteLine("and suddenly......quarks!");
+            quarks = quarks + 3;
+        }
+        //quark # getter
+        public double getQuarks()
+        {
+            return quarks;
+        }
+        //for creating atoms
+        public void Atoms()
+        {
+            if(quarks >= 3)
+            {
+                Console.WriteLine("woah hey look at the quarks, they're fusing!");
+                quarks = quarks - 3;
+                atoms = atoms + 1;
+                Console.WriteLine("now you have atoms!");
+            }
+            else
+            {
+                Console.WriteLine("oh you dont have enough quarks! atoms are made of these, you know.");
+            }
+        }
+        //atom # getter
+        public int getAtoms()
+        {
+            return atoms;
+        }
+        //make hydrogen
+        public void Hydrogen()
+        {
+            if (atoms >= 2)
+            {
+                Console.WriteLine("some of the atoms are doing a little dance!");
+                atoms = atoms - 2;
+                hydrogen = hydrogen + 1;
+            }
+            else
+            {
+                Console.WriteLine("hey, you need more atoms for that!");
+            }
+        }
+        //hydrogen # getter
+        public int getHydrogen()
+        {
+            return hydrogen;
+       }
+        //make helium
+        public void Helium()
+        {
+            if (atoms >= 2)
+            {
+                Console.WriteLine("some of the atoms are doing a little dance!");
+                atoms = atoms - 2;
+                helium = helium + 1;
+            }
+            else
+            {
+                Console.WriteLine("hey, you need more atoms for that!");
+            }
+        }
+        //hydrogen # getter
+        public int getHelium()
+        {
+            return helium;
+        }
+        //make stars, for every star created there should be +.5 quarks ,+1 helium , +.05 carbon every second
+        public void Star()
+        {
+            if(helium >= 10 && hydrogen >= 10)
+            {
+                Console.WriteLine("helium and hydrogen are dancing! jeez...it's so bright..");
+                helium = helium - 10;
+                hydrogen = hydrogen - 10;
+                star = star + 1;
+
+            }
+        }
+        public int getStar()
+        {
+            return star;
+        }
+        public void Resources()
+        {
+            Console.WriteLine(quarks + " quarks");
+            Console.WriteLine(atoms + " atoms");
+            Console.WriteLine(hydrogen + " hydrogen");
+            Console.WriteLine(helium + " helium");
+            Console.WriteLine(carbon + " carbon");
+        }
+        public void starResourceCounter()
+        {
+            while( star > 0)
+            {
+                helium++;
+                carbon = carbon + 0.05;
+                quarks = quarks + 0.5;
+                System.Threading.Thread.Sleep(1000);
+              /*  Console.WriteLine("--------------------");
+                Console.WriteLine("carbon " + carbon);
+                Console.WriteLine("helium " + helium);
+                Console.WriteLine("quarks " + quarks); */
+            }
+        }
+        //suppose to be a delegate + event for adding materials according to how many stars are owned
+        /*public int starAddedResources(int a)
+        {
+            for( star = 1; star > 0; helium++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                quarks = quarks + 0.5;
+                timerDelg starTimer = starAddedResources;
+            }
+            return star;
+        } */
+    }
+}
